e0f30d606d0cb49f60cd6f07eb33d05a638ca2b6
devtechkamal <131899275+devtechkamal@users.noreply.github.com>
2025-08-19 23:23:05 +0530

    Added NestJS API project

---
+++ api/prisma/migrations/20250819174417_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'USER',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
